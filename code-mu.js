cb4b7ea51a0def96fecaf734e98c3ad8cb567606
ArtemMyr <ar.muravyov01@yandex.ru>
2023-09-13 15:23:44 +0300

    calc

--- code-mu.js
+++ code-mu.js
@@ -1,3 +1,5 @@
+//215
+
 //Сделайте функцию func, которая будет возвращать через return какую-либо строку.
 // function func(){
 //     return 'hello'
@@ -69,3 +71,64 @@
 // }
 
 // console.log(func2()+func1());
+
+
+//
+
+
+// function test(num, func) {
+// 	console.log(func(num));
+// }
+
+// test(3, function(num){
+//     return num * num;
+// })
+
+// test(3, function(num){
+//     return num * num * num;
+// })
+
+
+// function test(arr, func){
+//     for (let i = 0; i < arr.length; i++) {
+// 		arr[i] = func(arr[i]);
+// 	}
+
+// 	return arr;
+// }
+
+// let result = test([1,2,3,4],function(num){
+//     return num * num
+// })
+
+// console.log(result)
+
+// function test(arr, func){
+//     for (let i = 0; i < arr.length; i++) {
+// 		arr[i] = func(arr[i]);
+// 	}
+
+// 	return arr;
+// }
+
+// let result = test([1,2,3,4],function(num){
+//     return num * num * num
+// })
+
+// console.log(result)
+
+//-----Вложенные функции-----\\
+// function func(num1, num2){
+//     function square(num){
+//         return num * num * num;
+//     }
+//     function cube(num){
+//         return num * num;
+//     }
+//     return square(num1) + cube(num2);
+// }
+
+// console.log(func(2, 3))
+
+//-----\\
+
