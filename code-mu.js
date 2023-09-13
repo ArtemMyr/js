621b5b1eb9e1922b3f24157c2c5da481e59b9dbb
ArtemMyr <ar.muravyov01@yandex.ru>
2023-09-13 11:37:44 +0300

    js

---
+++ code-mu.js
@@ -0,0 +1,71 @@
+//Сделайте функцию func, которая будет возвращать через return какую-либо строку.
+// function func(){
+//     return 'hello'
+// }
+// console.log(func())
+
+
+
+//Выведите результат работы функции func в консоль.
+// function func(a,b){
+//     return a+b
+// }
+// console.log(func(2,3))
+
+//Выведите исходный код func в консоль.
+// function func(){
+//     return '!'
+// }
+// console.log(func)
+
+
+//Запишите в переменную func число 123, тем самым затерев функцию из этой переменной. Выведите новое значение переменной func в консоль.
+// function func(){
+//     console.log('!');
+// }
+// func();
+
+// func = 123;
+// console.log(func);
+
+//Сделайте функцию func1, которая будет возвращать через return число 3.
+
+// function func1() {
+// 	return 3;
+// }
+
+// console.log(func1());
+
+//Скопируйте исходный код функции func1, в переменную func2.
+
+// function func1(){
+//     return 3;
+// }
+
+// let func2 = func1();
+
+// console.log(func2)
+
+//Выведите в консоль сумму результатов работы функций func1 и func2.
+// function func1(){
+//     return 3;
+// }
+
+// let func2 = func1();
+// console.log(func2+func1())
+
+//Сделайте безымянную функцию, которая будет возвращать через return число 1. Запишите эту функцию в переменную func1.
+
+// let func1 = function(){
+//     return 1;
+// }
+
+// console.log(func1());
+
+//Сделайте безымянную функцию, которая будет возвращать через return число 2. Запишите эту функцию в переменную func2.
+
+// let func2 = function(){
+//     return 2;
+// }
+
+// console.log(func2()+func1());
